7ec9837f72d9f4dd495d37602b8eb90ff48a2b66
Yurii Andrusiak <yurii.andrusiak@gmail.com>
2020-01-03 21:50:55 +0200

    set DEBUG false

--- perekazy/settings.py
+++ perekazy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#(cmn3bt(-(uy&cf+46iyb(##8id!e-6pr5^y%(6=m51*fa80_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
