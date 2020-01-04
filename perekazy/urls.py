b28701d3005161ce0b27d9a42a0e8ca4f646f908
Yurii Andrusiak <yurii.andrusiak@gmail.com>
2020-01-04 17:37:06 +0200

    Clear staticfiles/; Add favicon and dataTables

--- perekazy/urls.py
+++ perekazy/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from comparison.views import index as comparison_index
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('comparison/', include('comparison.urls')),
     path('admin/', admin.site.urls),
     url(r'^$', include ('comparison.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
